7c6e0a95f200b3840855aff19117b377179bdb09
Sai Krish Naidu <118285680+saikrish1105@users.noreply.github.com>
2025-10-30 03:27:12 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_YtXEFVGaD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'kro6k8lkbb8vdc5vhm6j2ps0h ', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
