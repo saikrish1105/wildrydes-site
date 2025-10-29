ef4b1357a1262a025fafbb29850bd195e6682d0f
Sai Krish Naidu <118285680+saikrish1105@users.noreply.github.com>
2025-10-30 04:04:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://kqsg5a9i19.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
